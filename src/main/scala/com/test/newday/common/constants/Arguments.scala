f9ef12346967f99c709eca31a9c39e78f26ef425
gsumar <gsalgadocea@gmail.com>
2017-10-08 19:27:46 +0100

    Refactor arguments.

---
+++ src/main/scala/com/test/newday/common/constants/Arguments.scala
@@ -0,0 +1,10 @@
+package com.test.newday.common.constants
+
+object Arguments {
+  val MoviesPath : Int = 0
+  val RatingsPath : Int = 1
+  val MoviesOutput : Int = 2
+  val RatingsOutput : Int = 3
+  val MoviesRatingsOutput : Int = 4
+  val UsersWith3FilmsOutput : Int = 5
+}
--- src/main/scala/com/test/newday/core/ProblemResolver.scala
+++ src/main/scala/com/test/newday/core/ProblemResolver.scala
@@ -1,6 +1,6 @@
 package com.test.newday.core
 
-import com.test.newday.common.constants.ArgumentsIndex
+import com.test.newday.common.constants.Arguments
 import com.test.newday.core.output.ParquetSaver
 import com.test.newday.core.reports.{MovieRatings, UserFavouritesFilms}
 import com.test.newday.core.tables.{BasicTableLoader, BasicTableType}
@@ -8,15 +8,15 @@ import com.test.newday.core.tables.{BasicTableLoader, BasicTableType}
 class ProblemResolver(args:Array[String]) {
 
   def run() = {
-    val movies = BasicTableLoader.load(args(ArgumentsIndex.moviesPath), BasicTableType.Movie)
-    val ratings = BasicTableLoader.load(args(ArgumentsIndex.ratingsPath), BasicTableType.Rating)
+    val movies = BasicTableLoader.load(args(Arguments.MoviesPath), BasicTableType.Movie)
+    val ratings = BasicTableLoader.load(args(Arguments.RatingsPath), BasicTableType.Rating)
     val moviesRatings = MovieRatings.register(movies, ratings)
     val usersWith3Films = UserFavouritesFilms.register(movies, ratings, moviesRatings)
 
-    ParquetSaver.save(movies, args(ArgumentsIndex.moviesOutput))
-    ParquetSaver.save(ratings, args(ArgumentsIndex.ratingsOutput))
-    ParquetSaver.save(moviesRatings, args(ArgumentsIndex.moviesRatingsOutput))
-    ParquetSaver.save(usersWith3Films, args(ArgumentsIndex.usersWith3FilmsOutput))
+    ParquetSaver.save(movies, args(Arguments.MoviesOutput))
+    ParquetSaver.save(ratings, args(Arguments.RatingsOutput))
+    ParquetSaver.save(moviesRatings, args(Arguments.MoviesRatingsOutput))
+    ParquetSaver.save(usersWith3Films, args(Arguments.UsersWith3FilmsOutput))
   }
 
 }
